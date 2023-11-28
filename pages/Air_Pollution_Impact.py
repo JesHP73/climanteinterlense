11d2d7f0648cbbc3e732eba5c31270b5ce4a651d
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-28 11:28:27 +0100

    Update Air_Pollution_Impact.py

--- pages/Air_Pollution_Impact.py
+++ pages/Air_Pollution_Impact.py
@@ -6,54 +6,32 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# WHO standards for pollutants
+WHO_STANDARDS = {
+    'PM10': {'AQG': 15, 'RL': 45},
+    'PM2.5': {'AQG': 5, 'RL': 15},
+    'O3': {'AQG': 60, 'RL': 100},
+    'NO2': {'AQG': 10, 'RL': 25},
+    'CO': {'AQG': 4, 'RL': 10}  # Assuming the unit is mg/m3 for simplicity
+}
+
 # Function to load data
 @st.cache
 def load_data():
     try:
         DATA_URL = 'https://raw.githubusercontent.com/JesHP73/climanteinterlense/main/dataset/socio_economical_agg_dataset.csv'
         data = pd.read_csv(DATA_URL)
+        # Assuming the dataset has columns 'year', 'pollutant' and 'emission'
         return data
     except Exception as e:
         st.error(f"Error loading data: {e}")
         return pd.DataFrame()  # Return an empty DataFrame in case of error
 
-# Page content function
-def show_air_pollution_impact(df):
-    if df.empty:
-        st.error("No data available to display.")
-        return
-
-    st.title("🌍 Air Pollution Impact")
-    st.write("Analyze the differential impact of air pollution based on demographic data.")
-
-    # Sidebar filters
-    selected_region = st.sidebar.multiselect('Select Region', options=df['region'].unique(), default=df['region'].unique())
-    selected_country = st.sidebar.multiselect('Select Country', options=df['country'].unique(), default=df['country'].unique())
-    selected_zone = st.sidebar.multiselect('Select Zone', options=df['zone'].unique(), default=df['zone'].unique())
-
-    # Data filtering based on sidebar selection
-    if not selected_region and not selected_country and not selected_zone:
-        filtered_data = df
-    else:
-        filtered_data = df[
-            (df['region'].isin(selected_region)) &
-            (df['country'].isin(selected_country)) &
-            (df['zone'].isin(selected_zone))
-        ]
-
-    # Plotting
-    if not filtered_data.empty:
-        fig, ax = plt.subplots()
-        sns.barplot(data=filtered_data, x='air_pollutant', y='avg_air_pollutant_level', hue='age_group', ax=ax)
-        plt.title("Impact of Air Pollution by Age Group")
-        plt.xlabel("Air Pollutant")
-        plt.ylabel("Average Air Pollutant Level")
-        st.pyplot(fig)
-    else:
-        st.write("No data to display. Please adjust the filter options.")
-
-# Load data
-df = load_data()
-
-# Call page content function
-show_air_pollution_impact(df)
+# Function to plot emissions data with WHO standards
+def plot_emissions(df, pollutant):
+    # Aggregate data to get annual averages for the selected pollutant
+    annual_data = df[df['pollutant'] == pollutant].groupby('year')['emission'].mean().reset_index()
+    
+    # Start plotting
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.lineplot(data=annual_data, x='year', y='emission', ax=ax, marker='o
