300aa4e510df75da4610f006abbc964a14815497
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-28 11:39:07 +0100

    Update Air_Pollution_Impact.py

--- pages/Air_Pollution_Impact.py
+++ pages/Air_Pollution_Impact.py
@@ -15,8 +15,34 @@ WHO_STANDARDS = {
 # Function to load data
 @st.cache
 def load_data():
-    # Your data loading logic here
-    # ...
+    try:
+        DATA_URL = 'https://raw.githubusercontent.com/JesHP73/climanteinterlense/main/dataset/socio_economical_agg_dataset.csv'
+        data = pd.read_csv(DATA_URL)
+        return data
+    except Exception as e:
+        st.error(f"Error loading data: {e}")
+        return pd.DataFrame()  # Return an empty DataFrame in case of an error.
+
+# Function to plot annual emissions and compare with WHO standards
+def plot_emissions(df, pollutant):
+    # Aggregate your data to get annual averages for the selected pollutant
+    annual_data = df[df['pollutant'] == pollutant].groupby('year')['emission'].mean().reset_index()
+
+    # Start plotting
+    plt.figure(figsize=(10, 5))
+    ax = sns.lineplot(x='year', y='emission', data=annual_data)
+
+    # WHO guidelines
+    plt.axhline(y=WHO_STANDARDS[pollutant]['AQG'], color='green', linestyle='--', label='WHO AQG')
+    plt.axhline(y=WHO_STANDARDS[pollutant]['RL'], color='red', linestyle='--', label='WHO RL')
+
+    # Enhance the plot
+    plt.title(f'Annual Average Emissions of {pollutant} (compared to WHO guidelines)')
+    plt.xlabel('Year')
+    plt.ylabel('Emission Level (μg/m3)')
+    plt.legend()
+
+    return plt
 
 # Page content function for Air Pollution Impact
 def air_pollution_impact(df):
@@ -30,13 +56,9 @@ def air_pollution_impact(df):
     # Sidebar filter to select pollutants
     selected_pollutant = st.sidebar.selectbox('Select Pollutant', options=list(WHO_STANDARDS.keys()))
 
-    # Data aggregation and plotting function
-    def plot_pollution_data(pollutant):
-        # Aggregate data and plot logic here
-        # ...
-
     # Call the plotting function
-    plot_pollution_data(selected_pollutant)
+    fig = plot_emissions(df, selected_pollutant)
+    st.pyplot(fig)
 
     # Additional explanations about AQGs and RLs
     st.markdown("### WHO Air Quality Guidelines (AQGs) and Reference Levels (RLs)")
