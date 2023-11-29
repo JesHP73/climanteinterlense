573e19e03ea35da7b2ac1a6c04bcb8bc826e0286
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-29 16:39:56 +0100

    Update Most_Polluted_Country.py

--- pages/Most_Polluted_Country.py
+++ pages/Most_Polluted_Country.py
@@ -1,1 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+
+# Function to load data
+@st.cache_data  # 👈 Add the caching decorator
+def load_data():
+     try:
+        URL = 'https://github.com/JesHP73/climanteinterlense/blob/7119b0a7a9d50d703a3f2f2e0a5ada59a377ea50/dataset/mapping.csv'
+        data = pd.read_csv(URL)
+        return data #= pickle.load(data, fix_imports=True, encoding='ASCII', errors='strict', buffers=None)
+     except Exception as e:
+         st.error(f"Error loading data: {e}")
+         return pd.DataFrame()  # Return an empty DataFrame in case of error
+
+# Main body of your Streamlit app
+def main():
+    # Load data
+    original_mapping = load_data()
+    df = original_mapping.copy()
+
+st.map(df, size=20, color='#0044ff')
+
+# This ensures the app runs when the script is executed
+if __name__ == "__main__":
+  
+    main()
