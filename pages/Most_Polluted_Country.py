d2e54890bbbcd1cf327e1a70eb6f444399f09caf
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-29 17:18:17 +0100

    Update Most_Polluted_Country.py

--- pages/Most_Polluted_Country.py
+++ pages/Most_Polluted_Country.py
@@ -18,9 +18,7 @@ def load_data():
 def main():
      original_mapping = load_data()
      df = original_mapping.copy()
-     
-     plot_data = (df, latitude='latitude', longitude='longitude', size='avg_AQI_Index', color=' ig_label')
-     return st.map(plot_data)
+     return st.map(df, latitude='latitude', longitude='longitude', size='15', color=' ig_label')
 
 
 # This ensures the app runs when the script is executed
