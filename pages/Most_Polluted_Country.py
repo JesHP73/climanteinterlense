4c383d3e26fa902d2791a5cd55592fb595f31d26
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-30 03:33:12 +0100

    Update Most_Polluted_Country.py

--- pages/Most_Polluted_Country.py
+++ pages/Most_Polluted_Country.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import plotly.express as px
 
 data = [
     {"country": "Turkiye", "latitude": 38.959759, "longitude": 34.924965, "avg_air_pollutant_level": 38.613282},
