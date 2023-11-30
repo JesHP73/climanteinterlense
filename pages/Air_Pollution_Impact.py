cf6c896bd9d63bafb61500830777ef0cfdb1ad46
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-30 12:06:52 +0100

    Update Air_Pollution_Impact.py

--- pages/Air_Pollution_Impact.py
+++ pages/Air_Pollution_Impact.py
@@ -8,201 +8,72 @@ import scipy
 import plotly.express as px
 
 
-# WHO standards for pollutants
-WHO_STANDARDS = {
-    'PM10': {'AQG': 15, 'RL': 45},
-    'PM2.5': {'AQG': 5, 'RL': 15},
-    'O3': {'AQG': 60, 'RL': 100},
-    'NO2': {'AQG': 10, 'RL': 25},
-    'CO': {'AQG': 4, 'RL': 10}  # Assuming the unit is mg/m3 for simplicity
-}
-
-# Function to get the standard value for a given pollutant and standard type
-def get_standard(pollutant, standard_type):
-    return WHO_STANDARDS.get(pollutant, {}).get(standard_type, None)
-
 # Function to load data
 @st.cache_data  # 👈 Add the caching decorator
 def load_data():
-     try:
+    try:
         URL = 'https://raw.githubusercontent.com/JesHP73/climanteinterlense/main/dataset/socio_economical_agg_dataset.csv'
         data = pd.read_csv(URL)
-        return data 
-     except Exception as e:
-         st.error(f"Error loading data: {e}")
-         return pd.DataFrame()  # Return an empty DataFrame in case of error
-
-
-
-
-def plot_emissions(df, selected_pollutants):
-    # Data Validation: Check if dataframe is empty
-    if df.empty:
-        st.error('No data available for the selected filters')
-        return
-
-    # Convert decades to string to avoid commas in the x-axis labels
-    df['decade'] = df['decade'].astype(str)
-    
-    # Add a new column for each pollutant indicating if it's above the WHO standard
-    for pollutant, standards in WHO_STANDARDS.items():
-        # Create a mask for rows where the current pollutant matches
-        mask = df['air_pollutant'] == pollutant
-        # Directly compare where the mask is True
-        df.loc[mask, f'{pollutant}_above_AQG'] = df.loc[mask, 'avg_air_pollutant_level'] > standards['AQG']
-    
-    # Now create a single column 'pollution_above_who' indicating if any pollutant is above its AQG
-    pollutants_above_columns = [f'{pollutant}_above_AQG' for pollutant in WHO_STANDARDS]
-    df['pollution_above_who'] = df[pollutants_above_columns].any(axis=1, skipna=True)
-
-    # This will hold the countries with the most pollution above WHO standard
-    countries_above_who = None
-
-    try:
-        if 'All' in selected_pollutants:
-            # Calculate the mean across all pollutants for each decade
-            overall_mean = df.groupby('decade')['avg_air_pollutant_level'].mean()
-
-            # Assuming AQG and RL are constants, use a representative value for all pollutants
-            aqg = sum([WHO_STANDARDS[pollutant]['AQG'] for pollutant in WHO_STANDARDS]) / len(WHO_STANDARDS)
-            rl = sum([WHO_STANDARDS[pollutant]['RL'] for pollutant in WHO_STANDARDS]) / len(WHO_STANDARDS)
-
-            # Create a DataFrame for plotting
-            plot_data = overall_mean.to_frame(name='Average Pollution Level')
-            plot_data['WHO AQG'] = aqg
-            plot_data['WHO RL'] = rl
-
-            # Plot the data using st.line_chart
-            st.line_chart(plot_data)
-
-        else:
-            # Initialize an empty DataFrame for joined data
-            joined_data = pd.DataFrame(index=df['decade'].unique())
-
-            # If specific pollutants are selected, filter and plot with WHO standards
-            for pollutant in selected_pollutants:
-                aqg = WHO_STANDARDS[pollutant]['AQG']
-                rl = WHO_STANDARDS[pollutant]['RL']
-
-                # Filter data for the current pollutant
-                pollutant_data = df[df['air_pollutant'] == pollutant]
-
-                # Calculate the average pollutant level per decade
-                avg_pollutant_data = pollutant_data.groupby('decade')['avg_air_pollutant_level'].mean().rename(f'Avg {pollutant} Pollution Level')
-                                                                                                               
-                # Join the average data with the joined_data DataFrame
-                joined_data = joined_data.join(avg_pollutant_data, how='left')
-
-                # Add the AQG and RL values as constant columns to joined_data
-                joined_data[f'{pollutant}_AQG'] = aqg
-                joined_data[f'{pollutant}_RL'] = rl
-
-            # Check if joined_data is empty after processing
-            if joined_data.empty:
-                st.error('No data available to plot after processing.')
-                return
-
-            # Plot the data using st.line_chart
-            st.line_chart(joined_data)
-
-            countries_above_who = df[df['pollution_above_who']].groupby('country').size().sort_values(ascending=False)
-            if not countries_above_who.empty:
-                most_polluted_country = countries_above_who.idxmax()
-                st.warning(f"The country with the most pollution above WHO standard is: {most_polluted_country}")
-
-            # If only one pollutant is selected, add a caption for clarity
-            if len(selected_pollutants) == 1:
-                st.caption(f'Line represents average levels of {selected_pollutants[0]} over time. '
-                           f'WHO AQG and RL for {selected_pollutants[0]} are also shown.')
+        return data
     except Exception as e:
-        st.error(f"An error occurred while plotting: {e}")
+        st.error(f"Error loading data: {e}")
+        return pd.DataFrame()  # Return an empty DataFrame in case of error
 
-    
 
 def main():
-    # Load data
+     # Load data
     original_data = load_data()
     df = original_data.copy()
     
     if df.empty:
         st.error("No data available to display.")
         return
-
-    st.title("Understanding Air Pollution and Its Impact")
-    st.write("This page provides a simple overview of key aspects of air pollution and its impact on climate change.")
-    
-    # Filters
-    region_options = ['All'] + sorted(df['region'].unique().tolist())
-    country_options = ['All'] + sorted(df['country'].unique().tolist())
-    pollutant_options = ['All'] + sorted(WHO_STANDARDS.keys())
-    
-    selected_region = st.sidebar.multiselect('Select Region', options=region_options, default=['All'])
-    selected_country = st.sidebar.multiselect('Select Country', options=country_options, default=['All'])
-    selected_pollutants = st.sidebar.multiselect('Select Pollutant(s)', options=pollutant_options, default=['All'])
-    
-    # Efficient combined filtering
-    conditions = []
-    if 'All' not in selected_region:
-        conditions.append(df['region'].isin(selected_region))
-    if 'All' not in selected_country:
-        conditions.append(df['country'].isin(selected_country))
-    if 'All' not in selected_pollutants:
-        conditions.append(df['air_pollutant'].isin(selected_pollutants))
-    
-    if conditions:
-        filtered_data = df[np.logical_and.reduce(conditions)]
-    else:
-        filtered_data = df
-
-    if filtered_data.empty:
-        st.error("No data available for the selected criteria.")
-        return
-
-   # Main Content - Three Columns
-    col1, col2, col3 = st.columns(3)
-    
-    with col1:
-        st.header("Avg. AQI")
-        avg_aqi = filtered_data['avg_AQI_Index'].mean()
-        col1.metric("Average AQI", f"{avg_aqi:.2f} μg/m3")
-    
-    with col2:
-        st.header("Point 2: Regional Impact")
-        # Show how selected region is affected
-        # Include relevant content or visualizations for regional impact here
-    
-    with col3:
-        st.header("Higher Income vs Air Quality")
-    
-        # Check if columns exist
-        if 'avg_GNI_PPP' in filtered_data.columns and 'avg_air_pollutant_level' in filtered_data.columns:
-            # Calculate correlation
-            correlation_gni_pollution = filtered_data[['avg_GNI_PPP', 'avg_air_pollutant_level']].corr().iloc[0, 1]
-            col3.metric("Economic Correlation", f"{correlation_gni_pollution:.2f}")
-            # Explanatory note about correlation
-            col3.caption("A positive value indicates that higher income correlates with higher air pollution levels, and vice versa.")
-        else:
-            col3.write("Data not available for correlation analysis.")
         
+    st.title('Percentage of Deaths Attributed to Air Pollution, 1990-2022')
 
-    # Call the plotting function and show the plot
-    plot_emissions(df, selected_pollutants) 
-        
-    # Additional explanations about AQGs and RLs
-    st.markdown("### Understanding the Numbers")
-                
-    st.info("The guidelines and reference levels from WHO are designed to keep air quality at a level that's safe for public health. When pollution levels go above these numbers, it can lead to health concerns for the population, especially vulnerable groups like children and the elderly.")
+    # Sidebar for filters
+    st.sidebar.header('Filters')
 
+    # Multiselect for Region and Country
+    regions = df['region'].unique()
+    selected_regions = st.sidebar.multiselect('Select Region', regions, default=regions)
 
+    countries = df[df['region'].isin(selected_regions)]['country'].unique()
+    selected_countries = st.sidebar.multiselect('Select Country', countries, default=countries)
 
-# This ensures the app runs when the script is executed
+    # Filter the DataFrame based on selections
+    filtered_df = df[df['region'].isin(selected_regions) & df['country'].isin(selected_countries)]
+
+    # Plotting
+    plot_data(filtered_df)
+
+    # Display statistics
+    display_statistics(filtered_df)
+
 if __name__ == "__main__":
-    # Adding a button to clear the cache
-    if st.sidebar.button('Clear Cache'):
-        st.legacy_caching.clear_cache()
-        st.success('Cache cleared!')
-
     main()
+
+def plot_data(filtered_df):
+    fig = px.line(filtered_df, x='year', y='total_death_attributed_sex_standarized',
+                  color='ig_label', title='Line Chart')
+    st.plotly_chart(fig)
+
+def display_statistics(filtered_df):
+    # Calculate the average percentage of deaths and the correlation
+    avg_death_percentage = filtered_df['total_death_attributed_sex_standarized'].mean()
+    correlation = filtered_df['GNI_per_capita_wb_Atlas_USD_EUR'].corr(filtered_df['total_death_attributed_sex_standarized'])
+
+    # Using columns to display the statistics
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.header("Key Fact")
+        st.metric(label="Average Deaths Attributed to Air Pollution", value=f"{avg_death_percentage:.2f}%")
+
+    with col2:
+        st.header("Correlation Analysis")
+        correlation_label = "Positive" if correlation > 0 else "Negative"
+        st.metric(label="Income vs. Death Correlation", value=f"{correlation:.2f}", delta=correlation_label)
 
 
 
