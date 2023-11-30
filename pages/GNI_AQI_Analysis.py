eb43b1573dadbe8c9a3b043c82b374ede3390923
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-30 23:53:05 +0100

    Update GNI_AQI_Analysis.py

--- pages/GNI_AQI_Analysis.py
+++ pages/GNI_AQI_Analysis.py
@@ -8,15 +8,6 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import numpy as np
 
-# WHO standards for pollutants
-WHO_STANDARDS = {
-    'PM10': {'AQG': 15, 'RL': 45},
-    'PM2.5': {'AQG': 5, 'RL': 15},
-    'O3': {'AQG': 60, 'RL': 100},
-    'NO2': {'AQG': 10, 'RL': 25},
-    'CO': {'AQG': 4, 'RL': 10}  # Assuming the unit is mg/m3 for simplicity
-}
-
 
 # Function to load data
 @st.cache
@@ -38,13 +29,10 @@ df = df_original.copy()
 # User input areas
 region_options = ['All'] + sorted(df['region'].unique().tolist())
 country_options = ['All'] + sorted(df['country'].unique().tolist())
-pollutants_options = ['All'] + sorted(df['air_pollutant'].unique().tolist())
 
 # Sidebar filters
 selected_region = st.sidebar.multiselect('Select Region', options=region_options, default=['All'])
 selected_country = st.sidebar.multiselect('Select Country', options=country_options, default=['All'])
-selected_pollutants = st.sidebar.multiselect('Select Pollutant', options=pollutants_options, default=['All'])
-selected_year = st.sidebar.slider('Select Year', min_value=int(df['year'].min()), max_value=int(df['year'].max()), value=int(df['year'].min()))
 
 # Efficient combined filtering
 conditions = []
@@ -52,9 +40,6 @@ if 'All' not in selected_region:
     conditions.append(df['region'].isin(selected_region))
 if 'All' not in selected_country:
     conditions.append(df['country'].isin(selected_country))
-if 'All' not in selected_pollutants:
-    conditions.append(df['air_pollutant'].isin(selected_pollutants))
-conditions.append(df['year'] == selected_year)
 
 if conditions:
     filtered_data = df[np.logical_and.reduce(conditions)]
@@ -69,39 +54,14 @@ def plot_aqi_vs_gni(data):
     ax.add_artist(legend1)
     ax.set_xlabel('GNI per Capita')
     ax.set_ylabel('AQI Index')
-    ax.set_title('GNI per Capita vs AQI Index')
+    ax.set_title('World Bank GNI per Capita vs AQI Index')
     return fig
+    
 # Assuming load_data() is defined elsewhere
 df_original = load_data()
 
 # Create a copy of the DataFrame for manipulation
 df = df_original.copy()
-
-# User input areas
-region_options = ['All'] + sorted(df['region'].unique().tolist())
-country_options = ['All'] + sorted(df['country'].unique().tolist())
-pollutants_options = ['All'] + sorted(df['air_pollutant'].unique().tolist())
-
-# Sidebar filters
-selected_region = st.sidebar.multiselect('Select Region', options=region_options, default=['All'])
-selected_country = st.sidebar.multiselect('Select Country', options=country_options, default=['All'])
-selected_pollutants = st.sidebar.multiselect('Select Pollutant', options=pollutants_options, default=['All'])
-selected_year = st.sidebar.slider('Select Year', min_value=int(df['year'].min()), max_value=int(df['year'].max()), value=int(df['year'].min()))
-
-# Efficient combined filtering
-conditions = []
-if 'All' not in selected_region:
-    conditions.append(df['region'].isin(selected_region))
-if 'All' not in selected_country:
-    conditions.append(df['country'].isin(selected_country))
-if 'All' not in selected_pollutants:
-    conditions.append(df['air_pollutant'].isin(selected_pollutants))
-conditions.append(df['year'] == selected_year)
-
-if conditions:
-    filtered_data = df[np.logical_and.reduce(conditions)]
-else:
-    filtered_data = df.copy()
 
 # Function for plotting AQI Index vs GNI per Capita
 def plot_aqi_and_gni_over_time(data):
@@ -109,7 +69,7 @@ def plot_aqi_and_gni_over_time(data):
         st.error('No data available for plotting.')
         return
 
-    # Convert 'year' to numeric if it's not already
+    # Convert 'year' to numeric
     if not pd.api.types.is_numeric_dtype(data['year']):
         data['year'] = pd.to_numeric(data['year'], errors='coerce')
 
@@ -129,7 +89,7 @@ def plot_aqi_and_gni_over_time(data):
             gni_per_capita = data['GNI_per_capita'].to_numpy()
             ax2 = ax1.twinx()
             ax2.plot(years, gni_per_capita, 'b-', label='GNI per Capita')
-            ax2.set_ylabel('GNI per Capita ($)', color='b')
+            ax2.set_ylabel('GNI per Capita (Eur)', color='b')
         except Exception as e:
             st.error(f'Error plotting GNI per Capita: {e}')
             return
