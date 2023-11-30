eeb8ac3c88373f958374b2a3e1728ea01caa8cb0
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-30 20:41:14 +0100

    Update GNI_AQI_Analysis.py

--- pages/GNI_AQI_Analysis.py
+++ pages/GNI_AQI_Analysis.py
@@ -17,10 +17,7 @@ WHO_STANDARDS = {
     'CO': {'AQG': 4, 'RL': 10}  # Assuming the unit is mg/m3 for simplicity
 }
 
-# Function to get the standard value for a given pollutant and standard type
-def get_standard(pollutant, standard_type):
-    return WHO_STANDARDS.get(pollutant, {}).get(standard_type, None)
-    
+
 # Function to load data
 @st.cache
 def load_data():
@@ -38,118 +35,49 @@ df_original = load_data()
 # Create a copy of the DataFrame for manipulation
 df = df_original.copy()
 
-def plot_emissions(df, selected_pollutants):
-    # Data Validation: Check if dataframe is empty
+# Sidebar - User input areas
+st.sidebar.header("Filters")
+selected_region = st.sidebar.selectbox('Select Region', ['All'] + sorted(df['region'].unique()))
+selected_country = st.sidebar.selectbox('Select Country', ['All'] + sorted(df['country'].unique()))
+selected_pollutants = st.sidebar.multiselect('Select Pollutant', ['All'] + sorted(df['air_pollutant'].unique()))
+selected_year = st.sidebar.selectbox('Select Year', ['All'] + sorted(df['year'].unique().astype(str)))
+
+# Filtering the data based on user selections
+filtered_data = df.copy()
+if selected_region != 'All':
+    filtered_data = filtered_data[filtered_data['region'] == selected_region]
+if selected_country != 'All':
+    filtered_data = filtered_data[filtered_data['country'] == selected_country]
+if selected_pollutants != ['All']:
+    filtered_data = filtered_data[filtered_data['air_pollutant'].isin(selected_pollutants)]
+if selected_year != 'All':
+    filtered_data = filtered_data[filtered_data['year'] == int(selected_year)]
+
+def plot_emissions(df):
+    # Check if dataframe is empty
     if df.empty:
-        st.error('No data available for the selected filters')
+        st.error('No data available for the selected filters.')
         return
 
-    # Convert decades to string to avoid commas in the x-axis labels
+    # Convert year to string for plotting purposes
     df['year'] = df['year'].astype(str)
-    
-    # Add a new column for each pollutant indicating if it's above the WHO standard
-    for pollutant, standards in WHO_STANDARDS.items():
-        # Create a mask for rows where the current pollutant matches
-        mask = df['air_pollutant'] == pollutant
-        # Directly compare where the mask is True
-        df.loc[mask, f'{pollutant}_above_AQG'] = df.loc[mask, 'air_pollutant_level'] > standards['AQG']
-    
-    # Now create a single column 'pollution_above_who' indicating if any pollutant is above its AQG
-    pollutants_above_columns = [f'{pollutant}_above_AQG' for pollutant in WHO_STANDARDS]
-    df['pollution_above_who'] = df[pollutants_above_columns].any(axis=1, skipna=True)
 
-    # This will hold the countries with the most pollution above WHO standard
-    countries_above_who = None
+    # Create a plot comparing each pollutant's level to WHO standards
+    fig, ax = plt.subplots()
+    for pollutant in WHO_STANDARDS.keys():
+        if pollutant in df['air_pollutant'].unique():
+            sns.lineplot(x='year', y='air_pollutant_level', data=df[df['air_pollutant'] == pollutant], ax=ax, label=pollutant)
+            ax.axhline(y=WHO_STANDARDS[pollutant]['AQG'], color='red', linestyle='--', label=f"{pollutant} WHO AQG")
+    ax.set_title('Pollutant Levels Over Time Compared to WHO Standards')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Pollutant Level')
+    ax.legend()
+    return fig
 
-    try:
-        if 'All' in selected_pollutants:
-            # Calculate the mean across all pollutants for each decade
-            overall_mean = df.groupby('year')['air_pollutant_level']
-
-            # Assuming AQG and RL are constants, use a representative value for all pollutants
-            aqg = sum([WHO_STANDARDS[pollutant]['AQG'] for pollutant in WHO_STANDARDS]) / len(WHO_STANDARDS)
-            rl = sum([WHO_STANDARDS[pollutant]['RL'] for pollutant in WHO_STANDARDS]) / len(WHO_STANDARDS)
-
-            # Create a DataFrame for plotting
-            plot_data = overall_mean.to_frame(name='Average Pollution Level')
-            plot_data['WHO AQG'] = aqg
-            plot_data['WHO RL'] = rl
-
-            # Plot the data using st.line_chart
-            st.line_chart(plot_data)
-
-        else:
-            # Initialize an empty DataFrame for joined data
-            joined_data = pd.DataFrame(index=df['year'].unique())
-
-            # If specific pollutants are selected, filter and plot with WHO standards
-            for pollutant in selected_pollutants:
-                aqg = WHO_STANDARDS[pollutant]['AQG']
-                rl = WHO_STANDARDS[pollutant]['RL']
-
-                # Filter data for the current pollutant
-                pollutant_data = df[df['air_pollutant'] == pollutant]
-
-                # Calculate the average pollutant level per decade
-                avg_pollutant_data = pollutant_data.groupby('year')['air_pollutant_level'].rename(f'Avg {pollutant} Pollution Level')
-                                                                                                               
-                # Join the average data with the joined_data DataFrame
-                joined_data = joined_data.join(avg_pollutant_data, how='left')
-
-                # Add the AQG and RL values as constant columns to joined_data
-                joined_data[f'{pollutant}_AQG'] = aqg
-                joined_data[f'{pollutant}_RL'] = rl
-
-            # Check if joined_data is empty after processing
-            if joined_data.empty:
-                st.error('No data available to plot after processing.')
-                return
-
-            # Plot the data using st.line_chart
-            st.line_chart(joined_data)
-
-            countries_above_who = df[df['pollution_above_who']].groupby('country').size().sort_values(ascending=False)
-            if not countries_above_who.empty:
-                most_polluted_country = countries_above_who.idxmax()
-                st.warning(f"The country with the most pollution above WHO standard is: {most_polluted_country}")
-
-            # If only one pollutant is selected, add a caption for clarity
-            if len(selected_pollutants) == 1:
-                st.caption(f'Line represents average levels of {selected_pollutants[0]} over time. '
-                           f'WHO AQG and RL for {selected_pollutants[0]} are also shown.')
-
-
-        # User input areas
-        region_options = ['All'] + sorted(df['region'].unique().tolist())
-        country_options = ['All'] + sorted(df['country'].unique().tolist())
-        pollutants_options = ['All'] + sorted(df['air_pollutant'].unique().tolist())
-        
-        # Sidebar filters
-        selected_region = st.sidebar.multiselect('Select Region', options=region_options, default=['All'])
-        selected_country = st.sidebar.multiselect('Select Country', options=country_options, default=['All'])
-        selected_pollutants = st.sidebar.multiselect('Select Pollutant', options=pollutants_options, default=['All'])
-        selected_year = st.sidebar.slider('Select Year', min_value=int(df['year'].min()), max_value=int(df['year'].max()), value=int(df['year'].min()))
-        
-        # Efficient combined filtering
-        conditions = []
-        if 'All' not in selected_region:
-            conditions.append(df['region'].isin(selected_region))
-        if 'All' not in selected_country:
-            conditions.append(df['country'].isin(selected_country))
-        if 'All' not in selected_pollutants:
-            conditions.append(df['air_pollutant'].isin(selected_pollutants))
-        conditions.append(df['year'] == selected_year)
-        
-        if conditions:
-            filtered_data = df[np.logical_and.reduce(conditions)]
-        else:
-            filtered_data = df.copy()
-        
-        # Sort the data for line plotting
-        filtered_data.sort_values(by='year', inplace=True)
-
- except Exception as e:
-        st.error(f"An error occurred while plotting: {e}")
+# Plot based on the filtered data
+if not filtered_data.empty:
+    fig = plot_emissions(filtered_data)
+    st.pyplot(fig)
 
 
 # Function for plotting AQI Index and GNI per Capita over time as lines
