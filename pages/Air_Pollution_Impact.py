53742401f67a224003d33313313771006247d678
Jes <143019142+JesHP73@users.noreply.github.com>
2023-12-01 22:37:35 +0100

    Update Air_Pollution_Impact.py

--- pages/Air_Pollution_Impact.py
+++ pages/Air_Pollution_Impact.py
@@ -136,9 +136,8 @@ def main():
         return
 
     # Additional explanations about AQGs and RLs
-    st.markdown("### Understanding What You See")
-    
-    st.caption("Here the term **Atributes**, meaning it was one of the attributed risk factors for a given disease or cause of death. There can be multiple risk factors for a given disease which corroborate or amplify one another when both are present. This means that in some cases, air pollution was not the only risk factor but one of several.")
+    with st.expander("### ⚠️ Understanding What You See"):
+        st.write("Here the term :green[Atributes], means it was one of the attributed risk factors for a given disease or cause of death. There can be multiple risk factors for a given disease which corroborate or amplify one another when both are present. This means that in some cases, air pollution was not the only risk factor but one of several.")
     
     # Plotting
     plot_data(filtered_data)
