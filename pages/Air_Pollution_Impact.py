da10a1a2396d1196016c2ebc52696d5a2b21b0df
Jes <143019142+JesHP73@users.noreply.github.com>
2023-12-01 22:33:14 +0100

    Update Air_Pollution_Impact.py

--- pages/Air_Pollution_Impact.py
+++ pages/Air_Pollution_Impact.py
@@ -158,5 +158,5 @@ with st.expander("👋 :green[HEY! WHAT YOU SHOULD KNOW ABOUT THIS DATA]"):
     st.write("This data comes from the most recent publication of the Global Burden of Disease study by the Institute for Health Metrics and Evaluation (IHME) in 2019 and the Global Terrorism Database.")
     st.write("These estimates assign each death a single cause, based on data on the ‘underlying cause of death’ listed on death certificates, verbal autopsies, and statistical modeling. This is a simplification, as people often have multiple diseases or injuries that contribute to their death, which may also be listed on death certificates.")
     st.write("This chart shows data on causes of death globally Filtered by Rei Name related to Air pollution and only Europe, from 1990 until 2019, the year before the Covid-19 pandemic started.")
-    st.link_button(":green[Data source: IHME, Global Burden of Disease (2019)🔗!]", "https://vizhub.healthdata.org/gbd-results/")
+    st.link_button(":blue[Data source: IHME, Global Burden of Disease (2019)🔗!]", "https://vizhub.healthdata.org/gbd-results/")
 
