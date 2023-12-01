f6aef9b7cece167da57e236696ebc456b8c7a6c9
Jes <143019142+JesHP73@users.noreply.github.com>
2023-12-01 19:04:24 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -21,7 +21,7 @@ def load_data():
 # Load data
 df = load_data()
 
-st.markdown("<h1 style='color:blue;text-align:center;'>Welcome to the Intersectional Climate Learning and Action Helper</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color:green;text-align:center;'>Welcome to the Intersectional Climate Learning and Action Helper</h1>", unsafe_allow_html=True)
 st.markdown("Hello, *World!* :earth_americas:", unsafe_allow_html=True)
 
 st.divider()
