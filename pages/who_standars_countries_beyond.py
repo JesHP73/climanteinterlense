9f2cf7414634deae464a689b9d4b3654a4c05076
Jes <143019142+JesHP73@users.noreply.github.com>
2023-12-04 22:26:05 +0100

    Update who_standars_countries_beyond.py

--- pages/who_standars_countries_beyond.py
+++ pages/who_standars_countries_beyond.py
@@ -57,7 +57,7 @@ def plot_data(df_filtered, who_standards, selected_pollutant):
     fig.update_layout(xaxis_tickangle=-45)
 
     # Set the y-axis type to linear (default)
-    fig.update_yaxes(type='linear')
+    fig.update_yaxes(type='linear', autorange=False, range=[0, 300]) 
 
     # Add a line for the WHO standard
     standard = who_standards[selected_pollutant]['annual']
