17571caf18b4c170be19df1e532ad349b2f0e6c1
Jes <143019142+JesHP73@users.noreply.github.com>
2023-12-05 00:03:14 +0100

    Update who_standars_countries_beyond.py

--- pages/who_standars_countries_beyond.py
+++ pages/who_standars_countries_beyond.py
@@ -69,7 +69,7 @@ def plot_data(df_mean_levels, who_standards, selected_pollutant):
     # Add an annotation for the WHO standard line next to the line
     fig.add_annotation(
         text=f"WHO {selected_pollutant} Annual Standard (μg/m³)",
-        x=36, y=10,
+        x=34, y=10,
         xanchor='left', yanchor='bottom',
         showarrow=False,
         font=dict(
